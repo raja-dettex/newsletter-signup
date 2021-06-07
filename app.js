359908ebd37e086ea72e6432f0681f2f9864ed1e
unknown <rk6091039@gmail.com>
2021-06-08 03:03:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,124 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:"true"}));
+
+app.get("/",function(req, res){
+    res.sendFile(__dirname+"/signup.html");
+});
+app.get("/failure",function(req, res){
+    res.sendFile(__dirname+"/failure.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+const lastName = req.body.lName;
+const Email = req.body.email;
+    console.log(firstName, lastName, Email);
+    if(!firstName || !lastName || !Email){
+        res.send("fail");
+        
+    }
+    
+   
+    const data = {
+        members:[
+            {
+                email_adress: Email,
+                status: "subsribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    }
+    const postData = JSON.stringify(data);
+
+    const options = {
+        url:"https://us1.api.mailchimp.com/3.0/lists/$62402dbff0",
+        method:"POST",
+        
+        Headers: {
+            "Authorization":"bearer $72ed8df3ae8a9d187a0b1bf347b12cdb-us1"
+        },
+        body:postData,
+    }
+
+    request(options, function(err, response, body){
+        
+        if(err){
+            
+            res.send("failure");
+        }
+        else{
+            if(response.statusCode===200){
+                res.send("success");
+            }
+            else{
+                
+                res.redirect("/failure");
+            }
+        }
+
+    });
+
+   
+
+});
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("server is running on 3000");
+});
+
+
+
+// const firstName = req.body.fName;
+// const lastName = req.body.lName;
+// const Email = req.body.email;
+// const Data = {
+//     members: [
+//         {
+//             email_adress: Email,
+//             status: "subscribed",
+//             merge_fields:{
+//                 FNAME: firstName,
+//                 LNAME: lastName,
+//             }
+//         }
+//     ]
+// };
+// const jsonData = JSON.stringify(Data);
+// const url = "https://us1.api.mailchimp.com/3.0/lists/62402dbff0";
+// const options = {
+//     method:"POST",
+// Auth:"raja:72ed8df3ae8a9d187a0b1bf347b12cdb-us1",
+// }
+
+//     https.request(url, options, function(response){
+//         console.log(response.statusCode);
+//         response.on("data", function(data){
+//             console.log(JSON.parse(data));
+//         });
+//     }); 
